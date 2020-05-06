6caad1d7a7645dbe0b392648fc00760bc9f1e911
Limor Ofri <limorofri7@gmail.com>
2020-05-06 12:49:39 +0300

    add confirmation modal

--- js/index.js
+++ js/index.js
@@ -2,11 +2,21 @@ console.log('index.js is connected to html');
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbygXx0Hc3E48mDgmW4g9qZJapBM32XpDROl6kCh53V0yzR5C0zH/exec';
 const form = document.forms['submit-to-google-sheet']
-    
+
 form.addEventListener('submit', e => {
     e.preventDefault()
+   
+    Swal.fire({
+        title: 'Success!',
+        text: 'Your details have been saved!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        confirmButtonColor: 'hsla(267, 10%, 52%, 1)'
+    })
+      
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => console.log('Success!', response))
     .catch(error => console.error('Error!', error.message))
-})
 
+    form.reset()
+})
